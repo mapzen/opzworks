f207ef0ea307acb6e95cf41e4c816f78a0bfb94c
Grant Heffernan <heffergm@gmail.com>
2016-01-11 11:33:13 -0500

    0.3.12

--- lib/opzworks/meta.rb
+++ lib/opzworks/meta.rb
@@ -1,5 +1,5 @@
 module OpzWorks
-  VERSION     = '0.3.11'
+  VERSION     = '0.3.12'
   AUTHORS     = ['Grant Heffernan', 'Mapzen']
   EMAIL       = ['grant@mapzen.com']
   DESCRIPTION = 'OpzWorks Utilities'
